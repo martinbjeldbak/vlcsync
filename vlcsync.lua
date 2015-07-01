5f8bd1d5e353aa1b5053441d5b8e4c204dd3094f
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-06-30 22:00:12 -0700

    Give up on playing-listener

--- vlcsync.lua
+++ vlcsync.lua
@@ -1,62 +1,59 @@
--- VLC callbacks
+-- vlcsync.lua -- VLC extension to sync playback between friends --
+--[[
+INSTALLATION:
+Put the file in the VLC subdir /lua/extensions, by default:
+* Windows (all users): %ProgramFiles%\VideoLAN\VLC\lua\extensions\
+* Windows (current user): %APPDATA%\VLC\lua\extensions\
+* Linux (all users): /usr/share/vlc/lua/extensions/
+* Linux (current user): ~/.local/share/vlc/lua/extensions/
+* Mac OS X (all users): /Applications/VLC.app/Contents/MacOS/share/lua/extensions/
+(create directories if they don't exist)
+Restart the VLC.
+Then you simply use the extension by going to the "View" menu and selecting it.
+--]]
 
+-- VLC callbacks
 function descriptor()
   return {
     title = "VLC Sync",
     version = "0.0.1",
     author = "Martin Bjeldbak Madsen",
-    url = "http://martinbmadsen.dk",
+    url = "https://github.com/martinbjeldbak/vlcsync",
     shortdesc = "VLCSync",
     description = "Sync VLC playback with your friends!",
-    -- capabilities = {"menu", "input-listener", "playing-listener"}
-    capabilities = {"input-listener", "meta-listener", "playing-listener"}
+    capabilities = {"menu", "input-listener", "playing-listener"}
   }
 end
 
+-- Called when extension is activated
 function activate()
-  -- extension starts
-  dbg("Welcome")
+  dbg("Starting")
 
   if vlc.input.is_playing() then
     dbg("Content playing")
-    item = vlc.input.item()
-    -- tprint(vlc.input.item().metas(item))
-    input = vlc.object.input()
-    jump_to(1800)
   else
     dbg("Content NOT playing")
   end
 end
 
-
+-- Called when extension is deactivated (i.e. done executing)
 function deactivate()
-  -- extension stops
   dbg("Shutting down!")
 end
 
+-- called when user clicks "x" at top right
 function close()
-   -- function triggered on dialog box close event
-   -- for example to deactivate extension on dialog box close:
    vlc.deactivate()
-end
-
-function meta_changed()
-  dbg("testing")
 end
 
 function input_changed()
   -- triggered by Start/Stop media input event
+  dbg("Input changed")
   return false
 end
 
 function playing_changed()
-  dbg("playing changed")
-  vlc.osd.message("Status:" .. tostring(stat))
-end
-
-function meta_changed()
-   -- related to capabilities={"meta-listener"} in descriptor()
-   -- triggered by available media input meta data?
+  -- should fire on play/pause but doesn't (at least on OS X 2.2.1
 end
 
 -- UTILITY FUNCTIONS
@@ -81,10 +78,23 @@ function tprint(tbl, indent)
   end
 end
 
+function elasped_time()
+  vcl.var.get(vlc.object.input(), "time")
+end
+
+function jump_to_min(min)
+  jump_to_sec(min * 60)
+end
+
+function jump_to_min_sec(min, sec)
+  jump_to_sec(min * 60 + sec)
+end
+
 function jump_to_sec(num_sec)
   vlc.var.set(vlc.object.input(), "time", num_sec)
 end
 
 function pause()
+  dbg("Shutting down")
   vlc.playlist.pause()
 end
